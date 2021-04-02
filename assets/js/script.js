1d03cfe15cfe19a6178bcfdd5eeb5d29303be47e
unknown <tgelety@gmail.com>
2021-04-02 13:58:11 -0700

    added-iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-//TODO: 
 var passwordPrompt = function () {
   // gets a value between 8-128 for variable passwordLength
   var passwordLength = window.prompt('How long of a password should I generate? tip: choose a number between 8-128.');
@@ -8,7 +7,7 @@ var passwordPrompt = function () {
 
   // START: if passwordLength is not a number between 8-128 recursive loop
   // FIXED: letters being included EVEN though the forum guy said javascript is dumb and if you perform math on it it will expect intergers if you perform operations! never trust a forum post 
-  if (passwordLength > 8 && passwordLength < 128) {
+  if (passwordLength > 7 && passwordLength < 129) {
 
     //  buncha boolean logic
     var passwordSpecial = window.confirm('Would you like special characters in your password? tip: OK for Yes and CANCEL for NO');
@@ -19,16 +18,23 @@ var passwordPrompt = function () {
 
     var passwordUpperCase = window.confirm('Would you like UPPERCASE characters in your password? tip: OK for Yes and CANCEL for NO');
 
+     //TODO: iterate on the value from the passwordLength
 
+    for(var i =0; i < passwordLength; i++){
+      console.log(i);
+    }
 
-  } else {
-    // FIXED: moved the recusion to the very end so everything is one big box and feels very awkward 
+  }
+   else {
+    // FIXED: moved the recursion to the very end so everything is one big box and feels very awkward 
     window.alert('please enter a value between 8 and 128');
     return (passwordPrompt);
     //  END: passwordLength recursive loop
   }
 }
+
 passwordPrompt()
+ 
 
 
 // Get references to the #generate element

fcd1a5de5e415610d847dbdc3cceada39b8c7706
unknown <tgelety@gmail.com>
2021-04-02 13:32:30 -0700

    fixed-recusion-loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,32 @@
 var passwordPrompt = function () {
   // gets a value between 8-128 for variable passwordLength
   var passwordLength = window.prompt('How long of a password should I generate? tip: choose a number between 8-128.');
+  // FIXED: stupid non-whole numbers exception
+  var passwordLength = parseInt(passwordLength);
 
   // START: if passwordLength is not a number between 8-128 recursive loop
-  if (passwordLength < 8 || passwordLength > 128) {
+  // FIXED: letters being included EVEN though the forum guy said javascript is dumb and if you perform math on it it will expect intergers if you perform operations! never trust a forum post 
+  if (passwordLength > 8 && passwordLength < 128) {
 
+    //  buncha boolean logic
+    var passwordSpecial = window.confirm('Would you like special characters in your password? tip: OK for Yes and CANCEL for NO');
+    
+    var passwordNumber = window.confirm('Would you like numerical characters in your password? tip: OK for Yes and CANCEL for NO');
+
+    var passwordLowerCase = window.confirm('Would you like lowercase characters in your password? tip: OK for Yes and CANCEL for NO');
+
+    var passwordUpperCase = window.confirm('Would you like UPPERCASE characters in your password? tip: OK for Yes and CANCEL for NO');
+
+
+
+  } else {
+    // FIXED: moved the recusion to the very end so everything is one big box and feels very awkward 
     window.alert('please enter a value between 8 and 128');
     return (passwordPrompt);
     //  END: passwordLength recursive loop
-
-  } else {
-
-
-    //  rest of the code here 
-    var passwordSpecial = window.confirm('Would you like special characters in your password? tip: OK for Yes and CANCEL for NO');
-    var passwordNumber = window.confirm('Would you like numerical characters in your password? tip: OK for Yes and CANCEL for NO');
-    var passwordLowerCase = window.confirm('Would you like lowercase characters in your password? tip: OK for Yes and CANCEL for NO');
-    var passwordUpperCase = window.confirm('Would you like UPPERCASE characters in your password? tip: OK for Yes and CANCEL for NO');
   }
 }
-passwordLength()
+passwordPrompt()
 
 
 // Get references to the #generate element
